a5da19f7e2bafe00a1e1bb0ce524a13c1fa57085
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-06-15 16:07:27 +0000

    Add smart contract Deployed it to rinkby

---
+++ contract/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
+
+// Whitelist Contract Address: 0x0E28838696f29FCeF68D186e2dB289719eA1C552
